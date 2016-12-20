219e2883df9951383198e0b7c6012d29fe9751e3
MarkLombardiNelson <marklombardinelson@gmail.com>
2016-12-20 13:09:35 -0500

    Attempt 1 Javascript HW

---
+++ code.js
@@ -0,0 +1,20 @@
+// 1. Assign the message "Hello, World!" to a variable.
+var message ="Hello, World";
+// 2. Assign a different string to a variable name of your choice.
+var name ="Mark";
+// 3. Assign a number to a variable.
+var number = 13;
+// 4. Use string concatenation to display the number from #3 in a string.
+function number("");
+// 5. Make an array of at least four of your favorite movies or books or bands.
+function bands[("Pink Floyd", "Rush", "Peter Frampton", "Meatloaf")];
+
+// 6. Make a object of information about yourself with at least four properties.
+function findmark() {
+  var name = ("Mark")
+  var number = ("123456")
+  var email = ("mark@mark.com")
+  var girlfriend = ("Christa")
+
+  return ("Contact" + name + "at" + number + "OR" + email + "if he's not there ask" + girlfriend)
+})
